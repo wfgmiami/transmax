23c0aeeb4e2af427308929341529737931051d80
wfgmiami <wfgmiami@yahoo.com>
2018-09-01 15:57:25 -0400

    db url

--- server/env/production.js
+++ server/env/production.js
@@ -5,7 +5,7 @@
     heroku (or whatever VPS used) configuration to be set in the
     applications environment, along with NODE_ENV=production
  */
-
+console.log('process.env.POSTGRESQL_URI', process.env.POSTGRESQL_URI)
 module.exports = {
     DATABASE_URI: process.env.POSTGRESQL_URI
 };
