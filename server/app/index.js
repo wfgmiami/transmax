8483e71f3681529374d2f44bb2e979665ba7c3bf
wfgmiami <wfgmiami@yahoo.com>
2018-09-01 17:44:35 -0400

    db url

--- server/app/index.js
+++ server/app/index.js
@@ -32,10 +32,11 @@ app.get('/*', function (req, res) {
 
 if (process.env.NODE_ENV === 'production') {
     // Serve any static files
-    app.use(express.static(path.join(__dirname, '../../','client/build/static')));
+    console.log('PATH...................', path.join(__dirname, '../../','client/build/static'))
+    app.use(express.static(path.join(__dirname, 'client/build')));
     // Handle React routing, return all requests to React app
     app.get('*', function(req, res) {
-      res.sendFile(path.join(__dirname, '../../client/build', 'index.html'));
+      res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
     });
 }
 
