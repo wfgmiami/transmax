d8dd728ba940380932a0bf02668132e1e6589429
wfgmiami <wfgmiami@yahoo.com>
2018-09-09 21:49:44 -0400

    test

--- server/app/routes/trip.js
+++ server/app/routes/trip.js
@@ -5,6 +5,7 @@ const router = new express.Router();
 const Trip = require('../db/models').Trip;
 
 router.get('/', (req, res, next) => {
+  console.log('HITTTING ROUTE GET............',req)
   Trip.findAll({})
     .then( trips => res.json( trips ))
     .catch( next )
