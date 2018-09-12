68d1507f0eb2d2a551bfc424f501e014f2d499ca
wfgmiami <alpenchev@yahoo.com>
2018-09-11 20:09:09 -0400

    Update trip.actions.js

--- client/src/store/actions/load/trip.actions.js
+++ client/src/store/actions/load/trip.actions.js
@@ -1,46 +1,52 @@
-import axios from 'axios';
+import axios from "axios";
+import { Datatable } from "../../../components/members/Datatable";
 
-export const GET_TRIP = 'GET TRIP';
-export const SET_TRIP = 'SET TRIP';
-export const UPDATE_TRIP = 'UPDATE TRIP';
-// export const SUCCESS_POST_TRIP = 'SUCCESS POST TRIP';
+export const GET_TRIP = "GET TRIP";
+export const SET_TRIP = "SET TRIP";
+export const UPDATE_TRIP = "UPDATE TRIP";
+export const SAVE_TRIPS = "SAVE TRIPS";
 
-export function getTrip(){
+export function getTrip() {
+  return dispatch =>
+    dispatch({
+      type: GET_TRIP,
+      payload: Datatable
+    });
+  // const getTrip = axios.get("/api/trip");
 
-    const getTrip = axios.get('/api/trip');
-
-    return (dispatch) =>
-        getTrip.then((response) =>
-            dispatch({
-                type: GET_TRIP,
-                payload: response.data
-            })
-        );
+  // return dispatch =>
+  //   getTrip.then(response =>
+  //     dispatch({
+  //       type: GET_TRIP,
+  //       payload: response.data
+  //     })
+  //   );
 }
 
-export function setTrip(trip){
-console.log('set trip called', trip)
-    return {
-        type: SET_TRIP,
-        trip
-    };
-
-
-
-    // const postTrip = axios.post('/api/trip',{ ...trip });
-    // return (dispatch) =>
-    //     postTrip.then((response) =>
-    //         dispatch({
-    //             type: SET_TRIP,
-    //             payload: response.data
-    //         })
-    //     );
+export function setTrip(trip) {
+  console.log("set trip called", trip);
+  return {
+    type: SET_TRIP,
+    trip
+  };
 }
 
-export function updateTrip(trips){
-    console.log('UPDATE trip called', trips)
-    return {
-        type: UPDATE_TRIP,
-        trips
-    };
+export function saveTrips(trips) {
+  console.log("trip.actions.js saveTrip trips ", trips);
+  const postTrip = axios.post("/api/trip", { ...trips });
+  return dispatch =>
+    postTrip.then(response =>
+      dispatch({
+        type: SAVE_TRIPS,
+        payload: response.data
+      })
+    );
+}
+
+export function updateTrip(trips) {
+  // console.log("trip.actions.js updateTrip trips ", trips);
+  return {
+    type: UPDATE_TRIP,
+    trips
+  };
 }
