da402d6af539f245ce256b41b492e80ab139e725
wfgmiami <wfgmiami@yahoo.com>
2018-09-11 21:39:26 -0400

    working checked

--- server/app/index.js
+++ server/app/index.js
@@ -11,6 +11,10 @@ if (process.env.NODE_ENV === 'production') {
     // Serve any static files
     app.use(express.static(path.join(__dirname, '../../','client/build')));
 
+    app.use('/api', function(req, res) {
+        res.sendFile(path.join(__dirname, '../../','client/build', 'index.html'));
+      });
+
     // Handle React routing, return all requests to React app
     app.get('*', function(req, res) {
       res.sendFile(path.join(__dirname, '../../','client/build', 'index.html'));
